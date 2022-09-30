c63a045157d8c82fdee1c759768df35b46fdec4d
Michael C. Grant <mgrant@anaconda.com>
2022-09-30 12:21:49 -0500

    first commit

---
+++ recipe/test_api.py
@@ -0,0 +1,15 @@
+import opentelemetry
+import opentelemetry.attributes
+import opentelemetry.baggage
+import opentelemetry.context
+import opentelemetry.metrics
+import opentelemetry.propagate
+import opentelemetry.propagators
+import opentelemetry.trace
+import opentelemetry.util
+import opentelemetry.environment_variables
+import opentelemetry.version
+import subprocess
+subprocess.run(['pip', 'check'])
+import sys
+sys.exit(-1)
